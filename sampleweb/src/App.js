1ce455a8d3fe34d75e7b60422867ffe2853ac08b
cucucucumber <zijian.song1996@gmail.com>
2021-10-03 23:31:46 -0700

    changed fonts and backgrounds

--- sampleweb/src/App.js
+++ sampleweb/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import ReactCanvasNest from 'react-canvas-nest';
+import Navigation from './Components/Navigation/Navigation';
+import ImageLinkForm from './Components/Images/ImageLinkForm';
+import Counter from './Components/Counter/Counter'
+import "@lottiefiles/lottie-player";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+        super();
+        this.state = {
+            follow: true
+        }
+    }
+
+  render(){
+    const { follow } = this.state;
+    return(
+      <div>
+          <Navigation />
+          <lottie-player
+          autoplay
+          loop
+          background="transparent"
+          src="https://assets3.lottiefiles.com/packages/lf20_fu0dfaqu.json"
+          style={{width: '150px', height: '150px'}}
+        ></lottie-player>
+        <ImageLinkForm />
+        <Counter />
+        {/*<ReactCanvasNest />*/}
+{/*        <Logo />
+        <FaceRecognition />*/}
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ sampleweb/src/Components/Counter/Counter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Counter = () => {
+	return (
+		<div>
+			<div className='f3 center'>
+				{'Dear Guest, you are the first one visiting my warm small website'}
+			</div>
+			<div className='f1 white center'>
+				{'I LOVE YOU'}
+			</div>
+		</div>
+	)
+}
+
+export default Counter;
